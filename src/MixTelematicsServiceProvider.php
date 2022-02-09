7633d531dbe89ef9e2475c8c5685cbf6346c6939
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2022-02-08 21:20:01 -0300

    :racehorse: Change to inject dependency Laravel

--- src/MixTelematicsServiceProvider.php
+++ src/MixTelematicsServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace Aislandener\MixTelematicsLaravel;
 
+use Aislandener\MixTelematicsLaravel\Services\ActiveEventsService;
+use Aislandener\MixTelematicsLaravel\Services\AssetService;
+use Aislandener\MixTelematicsLaravel\Services\DriversService;
+use Aislandener\MixTelematicsLaravel\Services\GroupService;
+use Aislandener\MixTelematicsLaravel\Services\TokenService;
 use Illuminate\Support\ServiceProvider;
 use JetBrains\PhpStorm\Pure;
 
@@ -10,6 +15,8 @@ class MixTelematicsServiceProvider extends ServiceProvider
 
     public function register()
     {
+        $this->injectDependency();
+
         $configPath = __DIR__ . '/../config/mixtelematics.php';
         $this->mergeConfigFrom($configPath, 'mixtelematics');
 
@@ -24,6 +31,43 @@ class MixTelematicsServiceProvider extends ServiceProvider
             $this->publishConfig();
             $this->loadMigrationsFrom(__DIR__. '/../database/migrations');
         }
+    }
+
+    /**
+     * @return void
+     */
+    private function injectDependency(): void
+    {
+        $services = [
+            GroupService::class,
+            DriversService::class,
+            AssetService::class,
+            ActiveEventsService::class
+        ];
+        $this->app->when($services)
+            ->needs('$clientName')
+            ->giveConfig('mixtelematics.clientName');
+        $this->app->when($services)
+            ->needs('$clientID')
+            ->giveConfig('mixtelematics.clientID');
+        $this->app->when($services)
+            ->needs('$clientSecret')
+            ->giveConfig('mixtelematics.clientSecret');
+        $this->app->when($services)
+            ->needs('$idBaseUrl')
+            ->giveConfig('mixtelematics.IDBaseUrl');
+        $this->app->when($services)
+            ->needs('$restBaseUrl')
+            ->giveConfig('mixtelematics.RestBaseUrl');
+        $this->app->when($services)
+            ->needs('$dynamixUserName')
+            ->giveConfig('mixtelematics.dynamixUserName');
+        $this->app->when($services)
+            ->needs('$dynamixUserPassword')
+            ->giveConfig('mixtelematics.dynamixUserPassword');
+        $this->app->when($services)
+            ->needs('$organisationId')
+            ->giveConfig('mixtelematics.organisationId');
     }
 
     /**
--- src/Services/TokenService.php
+++ src/Services/TokenService.php
@@ -2,36 +2,44 @@
 
 namespace Aislandener\MixTelematicsLaravel\Services;
 
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 use Jumbojett\OpenIDConnectClient;
 use Jumbojett\OpenIDConnectClientException;
 
 abstract class TokenService
 {
-    private $clientName;
-    private $clientID;
-    private $clientSecret;
-    private $idBaseUrl;
-    private $restBaseUrl;
-    private $dynamixUserName;
-    private $dynamixUserPassword;
-    private $token;
+    private string $clientName;
+    private string $clientID;
+    private string $clientSecret;
+    private string $idBaseUrl;
+    private string $restBaseUrl;
+    private string $dynamixUserName;
+    private string $dynamixUserPassword;
+    private mixed $token;
 
-    protected $http;
-    protected $organisationId;
+    protected PendingRequest $http;
+    protected mixed $organisationId;
 
     const SCOPE = "offline_access MiX.Integrate";
 
-    public function __construct()
+    public function __construct(string $clientName,
+                                string $clientID,
+                                string $clientSecret,
+                                string $idBaseUrl,
+                                string $restBaseUrl,
+                                string $dynamixUserName,
+                                string $dynamixUserPassword,
+                                mixed $organisationId)
     {
-        $this->clientName = config('mixtelematics.clientName');
-        $this->clientID = config('mixtelematics.clientID');
-        $this->clientSecret = config('mixtelematics.clientSecret');
-        $this->idBaseUrl = config('mixtelematics.IDBaseUrl');
-        $this->restBaseUrl = config('mixtelematics.RestBaseUrl');
-        $this->dynamixUserName = config('mixtelematics.dynamixUserName');
-        $this->dynamixUserPassword = config('mixtelematics.dynamixUserPassword');
-        $this->organisationId = config('mixtelematics.organisationId');
+        $this->clientName = $clientName;
+        $this->clientID = $clientID;
+        $this->clientSecret = $clientSecret;
+        $this->idBaseUrl = $idBaseUrl;
+        $this->restBaseUrl = $restBaseUrl;
+        $this->dynamixUserName = $dynamixUserName;
+        $this->dynamixUserPassword = $dynamixUserPassword;
+        $this->organisationId = $organisationId;
 
         $this->token = $this->getToken();
 
@@ -41,7 +49,7 @@ abstract class TokenService
     private function getToken()
     {
         $oidc = new OpenIDConnectClient(
-            provider_url: $this->idBaseUrl,client_id: $this->clientID, client_secret: $this->clientSecret
+            provider_url: $this->idBaseUrl, client_id: $this->clientID, client_secret: $this->clientSecret
         );
 
         $oidc->providerConfigParam([
